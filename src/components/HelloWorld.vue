c216ec48df7ab32810ecc157a343d4f727dfc6b6
DrishyaChuke <102606786+DrishyaChuke@users.noreply.github.com>
2022-05-10 15:14:03 +0545

    Add files via upload

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,293 @@
+<template>
+  <div>
+        
+        <header id="mu-header" class="" role="banner">
+          <div class="container">
+            <nav class="navbar navbar-default mu-navbar">
+            <div class="container-fluid">
+              
+              <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                  <span class="sr-only">Toggle navigation</span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+                </button>
+
+                <a class="navbar-brand" href="#">Drishya</a>
+
+              </div>
+
+              <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                  <ul class="nav navbar-nav mu-menu navbar-right">
+                    <li><a href="#">HOME</a></li>
+                    <li><a href="#mu-about">ABOUT</a></li>
+                    <li><a href="#mu-service">SERVICES</a></li>
+                    <li><a href="#mu-contact">CONTACT</a></li>
+                  </ul>
+              </div>
+            </div>
+          </nav>
+          </div>
+        </header>
+
+        <section id="mu-featured-slider">
+          <div class="container">
+            <div class="row">
+              <div class="col-md-12">
+                <div class="mu-featured-area">
+                  <h1>Drishya Lal Chuke</h1>
+
+                  <div class="mu-featured-slider-wrapper">
+                    <div class="mu-featured-slider" id="typed-strings">
+                      <p>I am <span>UI/UX Designer</span></p>
+                      <p>I am <span>Web Developer</span></p>
+                    </div>
+                    <span id="typed"></span>
+                  </div>
+                  
+                  <div class="mu-featured-content">
+                    <p>I've been learning and working on web technology for the last two years as most of my college projects are based on building web technologies such as HTML/CSS, Javascript. I worked on Javascript, Bootstrap and RESTful API using flask as my final year project in bachelors I had worked on these libraries. Since then I have been studying with react libraries, Angularjs and Vuejs Framework. I’m also currently studying a course for full-stack web developer via MERN from Deerwalk Institution of Technology.</p>
+                    <a href="#mu-portfolio" class="mu-primary-btn view-my-work-btn">VIEW MY WORK</a>
+                  </div>
+
+                </div>
+              </div>			
+            </div>
+          </div>
+        </section>
+                
+        <main role="main">
+
+          <section id="mu-about">
+            <div class="container">
+              <div class="row">
+                <div class="col-md-12">
+                  <div class="mu-about-area">
+
+                    <div class="mu-about-header">
+                      <h2 class="mu-heading-title">ABOUT ME</h2>
+                      <span class="mu-header-dot"></span>
+                      <p>My name is Drishya Lal Chuke. I am a <strong>Web Designer</strong> and <strong>Web Developer</strong>.I'm a team-player, creative thinker, inquisitive, dedicated towards my goals and have a great enthusiasm for the profession. I’m honest regarding my ability to code and I’m problem solver so I get the work done.Still learning to be a better developer.</p>
+                    </div>
+                    
+                    
+                    <div class="mu-about-content">
+                      <div class="row">
+                        <div class="col-md-6 border border-success rounded">
+                          <div class="mu-about-content-left">
+                            <img class="mu-profile-img" src="assets/images/CV.jpg" alt="Resume">
+                          </div>
+                        </div>
+                        <div class="col-md-6">
+                          <div class="mu-about-content-right">
+            
+                                      <div class="mu-skill-progress-bar">
+                                        <h3>Skills</h3>
+                                        <span>Html5</span>
+                                        <div class="mu-pro-bar mu-html5-bar">
+                                          
+                                        </div>
+                                        <span>css</span>
+                                        <div class="mu-pro-bar mu-css-bar">
+                                        
+                                        </div>
+                                        <span>Wordpress</span>
+                                        <div class="mu-pro-bar mu-wordpress-bar">
+                                        </div>
+                                        <span>Photoshop</span>
+                                        <div class="mu-pro-bar mu-photoshop-bar">
+                                        </div>
+                                        <span>jQuery</span>
+                                        <div class="mu-pro-bar mu-jquery-bar">
+                                        </div>
+                                      </div>
+
+                                      
+                                      <div class="mu-social-media">
+                                        <h3>Social</h3>
+                                        <a href="#"><i class="fa fa-facebook"></i></a>
+                                        <a href="#"><i class="fa fa-twitter"></i></a>
+                                        <a href="#"><i class="fa fa-linkedin"></i></a>
+                                        <a href="#"><i class="fa fa-google-plus"></i></a>
+                                      </div>
+
+                                      
+                                      <a href="#" class="mu-resume-btn">Download My Resume</a>
+
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+          
+          <section id="mu-service">
+            <div class="container">
+              <div class="row">
+                <div class="col-md-12">
+                  <div class="mu-service-area">
+
+                    <div class="mu-service-header">
+                      <h2 class="mu-heading-title">MY SERVICES</h2>
+                      <span class="mu-header-dot"></span>
+                    </div>
+
+                    <div class="mu-service-content">
+                      <div class="row">
+
+                        
+                        <div class="col-md-4 col-sm-6">
+                          <div class="mu-service-content-single">
+                            <span class="mu-service-icon-box">
+                              <i class="fa fa-leaf"></i>
+                            </span>
+                            <h4>UI/UX Design</h4>
+                          </div>
+                        </div>
+                        
+                        <div class="col-md-4 col-sm-6">
+                          <div class="mu-service-content-single">
+                            <span class="mu-service-icon-box">
+                              <i class="fa fa-desktop"></i>
+                            </span>
+                            <h4>Web Development</h4>
+                          </div>
+                        </div>
+                        
+                        <div class="col-md-4 col-sm-6">
+                          <div class="mu-service-content-single">
+                            <span class="mu-service-icon-box">
+                              <i class="fa fa-briefcase"></i>
+                            </span>
+                            <h4>Branding</h4>
+                          </div>
+                        </div>
+                        
+                        <div class="col-md-4 col-sm-6">
+                          <div class="mu-service-content-single">
+                            <span class="mu-service-icon-box">
+                              <i class="fa fa-camera"></i>
+                            </span>
+                            <h4>Photography</h4>
+                          </div>
+                        </div>
+                        
+                        <div class="col-md-4 col-sm-6">
+                          <div class="mu-service-content-single">
+                            <span class="mu-service-icon-box">
+                              <i class="fa fa-android"></i>
+                            </span>
+                            <h4>Apps Development</h4>
+                          </div>
+                        </div>
+                        
+                        <div class="col-md-4 col-sm-6">
+                          <div class="mu-service-content-single">
+                            <span class="mu-service-icon-box">
+                              <i class="fa fa-line-chart"></i>
+                            </span>
+                            <h4>Digital Marketing</h4>
+                          </div>
+                        </div>
+
+                      </div>
+                    </div>
+                    
+
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+         
+          <section id="mu-contact">
+            <div class="container">
+              <div class="row">
+                <div class="col-md-12">
+                  <div class="mu-contact-area">
+
+                    <div class="mu-contact-header">
+                      <h2 class="mu-heading-title">CONTACT ME</h2>
+                      <span class="mu-header-dot"></span>
+                    </div>
+
+                    <div class="mu-contact-content">
+                      <div class="row">
+
+                        <div class="col-md-12">
+                          <div class="mu-contact-top">
+
+                            <div class="row">
+                              <div class="col-sm-4">
+                                <div class="mu-contact-top-single">
+                                  <div class="mu-icon"><i class="fa fa-map-marker"></i></div>
+                                  <p>Adwait Marg-28, Bagbazar, Kathmandu, Nepal</p>
+                                </div>
+                              </div>
+                              <div class="col-sm-4">
+                                <div class="mu-contact-top-single">
+                                  <div class="mu-icon"><i class="fa fa-phone"></i></div>
+                                  <p>+977 9861539200</p>
+                                </div>
+                              </div>
+                              <div class="col-sm-4">
+                                <div class="mu-contact-top-single">
+                                  <div class="mu-icon"><i class="fa fa-envelope"></i></div>
+                                  <p>drishyachuke98@gmail.com</p>
+                                </div>
+                              </div>
+
+                            </div>
+                          </div>
+                        </div>	
+
+                        <div class="col-md-12">
+                          <div class="mu-contact-bottom">
+                          <div id="form-messages"></div>
+                            <form id="ajax-contact" method="post" action="mailer.php" class="mu-contact-form">
+                              <div class="form-group">                
+                                <input type="text" class="form-control" placeholder="Name" id="name" name="name" required>
+                              </div>
+                              <div class="form-group">                
+                                <input type="email" class="form-control" placeholder="Enter Email" id="email" name="email" required>
+                              </div>              
+                              <div class="form-group">
+                                <textarea class="form-control" placeholder="Message" id="message" name="message" required></textarea>
+                              </div>
+                              <button type="submit" class="mu-send-msg-btn"><span>SUBMIT</span></button>
+                                  </form>
+                          </div>
+                        </div>	
+
+                      </div>
+                    </div>
+
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+
+        </main>
+        
+        
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
